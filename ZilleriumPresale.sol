60de8f7c6116e362302c042a0a5ba406b542d4ff
Trevor <sales@merrows.co.uk>
2017-09-02 10:27:38 +0100

    add

--- ZilleriumPresale.sol
+++ ZilleriumPresale.sol
@@ -65,11 +65,10 @@ contract Ownable {
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
    */
-  function transferOwnership(address newOwner) onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
-  }
+function transferOwnership(address newOwner) onlyOwner {
+    require(newOwner != address(0));        
+    owner = newOwner;
+ }
 
 }
 
